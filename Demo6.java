e08e4fbf23d352e3f2659f3d109e2e2b6b9a1a86
N3H4R4N1 <145921302+N3H4R4N1@users.noreply.github.com>
2023-09-24 21:44:52 +0530

    Add files via upload

---
+++ Demo6.java
@@ -0,0 +1,14 @@
+class Demo6
+{
+void m1(){
+System.out.println("Hello");
+}
+}
+class Test extends Demo6{
+static void m1(){
+System.out.println("Hi");
+}
+public static void main(String args[])
+{
+}
+}
